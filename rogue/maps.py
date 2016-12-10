7e868464c04e8f9f08ed9005eb5a2f8c4057e3ef
gian <g.wetten@bluewin.ch>
2016-12-10 15:28:56 +0100

    Dungeon generation + FOV

--- rogue/maps.py
+++ rogue/maps.py
@@ -1,3 +1,10 @@
+from random import randint
+
+ROOM_MAX_SIZE = 10
+ROOM_MIN_SIZE = 6
+MAX_ROOMS = 30
+
+
 class Tile:
     # a tile of the map and its properties
     def __init__(self, blocked, block_sight=None):
@@ -17,11 +24,23 @@ class Rect:
         self.x2 = x + w
         self.y2 = y + h
 
+    def center(self):
+        center_x = (self.x1 + self.x2) // 2
+        center_y = (self.y1 + self.y2) // 2
+        return (center_x, center_y)
+
+    def intersect(self, other):
+        # returns true if this rectangle intersects with another one
+        return (self.x1 <= other.x2 and self.x2 >= other.x1 and
+                self.y1 <= other.y2 and self.y2 >= other.y1)
+
 
 class Map(list):
 
     def __init__(self, height, width):
         list.__init__(self, [[Tile(True) for y in range(height)] for x in range(width)])
+        self.width = width
+        self.height = height
         self.create_standard_map()
 
     def create_room(self, room):
@@ -32,11 +51,81 @@ class Map(list):
                 self[x][y].block_sight = False
 
     def create_standard_map(self):
-        # create two rooms
-        room1 = Rect(20, 15, 10, 15)
-        room2 = Rect(50, 15, 10, 15)
-        self.create_room(room1)
-        self.create_room(room2)
+        rooms = []
+        num_rooms = 0
 
-        self.spawn_x = 25
-        self.spawn_y = 23
+        for r in range(MAX_ROOMS):
+            # random width and height
+            w = randint(ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+            h = randint(ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+            # random position without going out of the boundaries of the map
+            x = randint(0, self.width-w-1)
+            y = randint(0, self.height-h-1)
+
+            # "Rect" class makes rectangles easier to work with
+            new_room = Rect(x, y, w, h)
+
+            # run through the other rooms and see if they intersect with this one
+            failed = False
+            for other_room in rooms:
+                if new_room.intersect(other_room):
+                    failed = True
+                    break
+
+            if not failed:
+                # this means there are no intersections, so this room is valid
+
+                # "paint" it to the map's tiles
+                self.create_room(new_room)
+
+                # center coordinates of new room, will be useful later
+                (new_x, new_y) = new_room.center()
+
+                if num_rooms == 0:
+                    # this is the first room, where the player starts at
+                    self.spawn_x = new_x
+                    self.spawn_y = new_y
+
+                else:
+                    # all rooms after the first:
+                    # connect it to the previous room with a tunnel
+
+                    # center coordinates of previous room
+                    (prev_x, prev_y) = rooms[num_rooms-1].center()
+
+                    # draw a coin (random number that is either 0 or 1)
+                    if randint(0, 1):
+                        # first move horizontally, then vertically
+                        self.create_h_tunnel(prev_x, new_x, prev_y)
+                        self.create_v_tunnel(prev_y, new_y, new_x)
+                    else:
+                        # first move vertically, then horizontally
+                        self.create_v_tunnel(prev_y, new_y, prev_x)
+                        self.create_h_tunnel(prev_x, new_x, new_y)
+
+                # finally, append the new room to the list
+                rooms.append(new_room)
+                num_rooms += 1
+
+    def create_h_tunnel(self, x1, x2, y):
+        for x in range(min(x1, x2), max(x1, x2) + 1):
+            self[x][y].blocked = False
+            self[x][y].block_sight = False
+
+    def create_v_tunnel(self, y1, y2, x):
+        # vertical tunnel
+        for y in range(min(y1, y2), max(y1, y2) + 1):
+            self[x][y].blocked = False
+            self[x][y].block_sight = False
+
+    def is_visible_tile(self, x, y):
+        if x >= self.width or x < 0:
+            return False
+        elif y >= self.height or y < 0:
+            return False
+        elif self[x][y].blocked:
+            return False
+        elif self[x][y].block_sight:
+            return False
+        else:
+            return True
--- rogue/rogue.py
+++ rogue/rogue.py
@@ -6,13 +6,17 @@ SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
 LIMIT_FPS = 20
 
-MAP_WIDTH = 80
-MAP_HEIGHT = 45
+
+FOV_ALGO = 'BASIC'  # default FOV algorithm
+FOV_LIGHT_WALLS = True
+TORCH_RADIUS = 10
 
 root = tdl.init(SCREEN_WIDTH, SCREEN_HEIGHT, title="Roguelike", fullscreen=False)
+fov_recompute = True
 
 
 def handle_keys(player, current_map):
+    global fov_recompute
     user_input = tdl.event.key_wait()
 
     if user_input.key == 'ENTER' and user_input.alt:
@@ -24,26 +28,47 @@ def handle_keys(player, current_map):
     # movement keys
     if user_input.key == 'UP':
         player.move(0, -1, current_map)
+        fov_recompute = True
     elif user_input.key == 'DOWN':
         player.move(0, 1, current_map)
+        fov_recompute = True
     elif user_input.key == 'LEFT':
         player.move(-1, 0, current_map)
+        fov_recompute = True
     elif user_input.key == 'RIGHT':
         player.move(1, 0, current_map)
+        fov_recompute = True
 
 
 def render_all(console, current_map, objects):
+    global fov_recompute
     color_dark_wall = (0, 0, 100)
+    color_light_wall = (130, 110, 50)
     color_dark_ground = (50, 50, 150)
-
-    # go through all tiles, and set their background color
-    for y in range(MAP_HEIGHT):
-        for x in range(MAP_WIDTH):
-            wall = current_map[x][y].block_sight
-            if wall:
-                console.draw_char(x, y, None, fg=None, bg=color_dark_wall)
-            else:
-                console.draw_char(x, y, None, fg=None, bg=color_dark_ground)
+    color_light_ground = (200, 180, 50)
+    if fov_recompute:
+        fov_recompute = False
+        visible_tiles = tdl.map.quickFOV(objects[0].x, objects[0].y,
+                                         current_map.is_visible_tile,
+                                         fov=FOV_ALGO,
+                                         radius=TORCH_RADIUS,
+                                         lightWalls=FOV_LIGHT_WALLS)
+        # go through all tiles, and set their background color
+        for y in range(current_map.height):
+            for x in range(current_map.width):
+                visible = (x, y) in visible_tiles
+                wall = current_map[x][y].block_sight
+                if not visible:
+                    # it's out of the player's FOV
+                    if wall:
+                        console.draw_char(x, y, None, fg=None, bg=color_dark_wall)
+                    else:
+                        console.draw_char(x, y, None, fg=None, bg=color_dark_ground)
+                else:
+                    if wall:
+                        console.draw_char(x, y, None, fg=None, bg=color_light_wall)
+                    else:
+                        console.draw_char(x, y, None, fg=None, bg=color_light_ground)
 
     # draw all objects in the list
     for obj in objects:
@@ -60,8 +85,8 @@ def main():
 
     player = GameObject(SCREEN_WIDTH//2, SCREEN_HEIGHT//2, '@', (255, 255, 255), con)
     npc = GameObject(SCREEN_WIDTH//2 - 5, SCREEN_HEIGHT//2, '@', (255, 255, 0), con)
-    objects = [npc, player]
-    start_map = Map(MAP_HEIGHT, MAP_WIDTH)
+    objects = [player, npc]
+    start_map = Map(45, 80)
     player.x = start_map.spawn_x
     player.y = start_map.spawn_y
 
